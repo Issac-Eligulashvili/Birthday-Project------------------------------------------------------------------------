01a00a17b72da5faf6d54499650e5e15e656e416
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-02-01 20:15:47 -0500

    added sounds effects/hologram

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ document.addEventListener('keydown', function(event) {
         subtext.classList.add('hide');
     }
   });
+
+let screenCont = document.querySelector("#screenContainer");
+let showing = false
+
+function show1() {
+    let infoDiv1 = document.querySelector('#info1');
+    if (showing === false) {
+    screenCont.classList.remove('hideCont');
+    screenCont.classList.add('showCont');  
+    showing = true; 
+    let revealSound = new Audio("/sounds/reveal.mp3");
+    revealSound.play()
+    revealSound.volume = 0.2
+
+    } else {
+        screenCont.classList.remove('showCont');
+        screenCont.classList.add('hideCont');  
+        showing = false;
+        let hideSound = new Audio("/sounds/hide.mp3");
+    hideSound.play()
+    hideSound.volume = 0.2
+    }
+    
+}
+
